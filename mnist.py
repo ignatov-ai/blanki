af4738f688ec2f6f2d7b94bcebc952506baa58ee
ignatov-ai <81515057+ignatov-ai@users.noreply.github.com>
2022-05-29 00:51:29 +0300

    work_rotation

--- mnist.py
+++ mnist.py
@@ -1,3 +1,6 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 import keras
 from keras.datasets import mnist
 from keras.models import Sequential
@@ -28,15 +31,15 @@ print(x_train.shape[0], 'Размер train')
 print(x_test.shape[0], 'Размер test')
 
 batch_size = 64
-epochs = 10
+epochs = 15
 
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),activation='sigmoid',input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='sigmoid'))
+model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 model.add(Flatten())
-model.add(Dense(256, activation='sigmoid'))
+model.add(Dense(256, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
 model.compile(loss=keras.losses.categorical_crossentropy,optimizer=keras.optimizers.Adadelta(),metrics=['accuracy'])
